f823a8273be5cd84a1fbe5e1210dc8a981f27a33
chandan mahajan <mahajan.chandan18@gmail.com>
2020-08-15 20:58:27 -0500

    refined setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
-from setuptools import setup
-try: # for pip >= 10
-    from pip._internal.req import parse_requirements
-    from pip._internal.download import PipSession
-except ImportError: # for pip <= 9.0.3
-    from pip.req import parse_requirements
-    from pip.download import PipSession
+import io
+import os
+from setuptools import setup, find_packages
 
 
-install_reqs = parse_requirements('requirements.txt', session=PipSession())
+def read(*filenames, **kwargs):
+    encoding = kwargs.get("encoding", "utf-8")
+    sep = kwargs.get("sep", os.linesep)
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
 
-reqs = [str(ir.req) for ir in install_reqs]
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+def read_requirements():
+    content = read("requirements.txt")
+    # Strip commented lines.
+    return [line for line in content.split(os.linesep) if not line.strip().startswith("#")]
 
 setup(
    name='downloads3key',
@@ -21,9 +25,9 @@ setup(
    author='Chandan Mahajan',
    author_email='mahajan.chandan18@gmail.com',
    packages=['downloads3key'],  #same as name
-   install_requires=reqs, #external packages as dependencies
+   install_requires=read_requirements(), #external packages as dependencies
    license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-   long_description=long_description,
+   long_description=read("README.md"),
    long_description_content_type="text/markdown",
    keywords = ['aws s3', 'download s3 key versions'],   # Keywords that define your package best
    python_requires='>=3.6',
@@ -31,5 +35,16 @@ setup(
           'console_scripts': [
               'downloads3key = downloads3key.__main__:main'
           ]
-      }
+      },
+    classifiers=[
+        "Development Status :: Beta",
+        "Environment :: Console",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "License :: MIT",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Utilities",
+    ]
 )
