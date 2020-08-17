901da4f145405f290e403a5f149bada4827bd4b5
chandan mahajan <mahajan.chandan18@gmail.com>
2020-08-16 23:51:56 -0500

    refactored output file name as file-name_version_lastmodified

--- downloads3key/cli.py
+++ downloads3key/cli.py
@@ -17,7 +17,6 @@ def print_info(ctx, param, value):
 @click.version_option(version=__version__, prog_name="Download s3 key cli")
 @click.option("--info", is_flag=True, is_eager=True, callback=print_info, expose_value=False)
 def download(bucket, key):
-    profile_name = 'default'
     session = boto3.Session()
     s3_client = session.client('s3')
     bucket_name = bucket;
@@ -45,15 +44,15 @@ def download(bucket, key):
     head, file_name = os.path.split(key)
 
     for page in page_iterator or []:
-        versions = page["Versions"] or []
+        versions = page['Versions'] or []
         for obj in versions:
             i = i + 1
-            version_id = ''
             actual_version_id = obj.get('VersionId')
+            version_id = ''
             if actual_version_id != 'null':
                 version_id = actual_version_id
-            output_file = output_folder_path + path_separator + str(
-                obj.get('LastModified').date().isoformat()) + "_" + version_id + file_name
-            s3_client.download_file(bucket_name, key, output_file, ExtraArgs={"VersionId": version_id})
+            output_file = output_folder_path + path_separator + file_name + '_' + version_id + '_'+str(
+                obj.get('LastModified').date().isoformat()) 
+            s3_client.download_file(bucket_name, key, output_file, ExtraArgs={"VersionId": actual_version_id})
 
     print("Total Files: " + str(i))
